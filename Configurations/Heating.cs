027ab3f7bbe3db1cdb8f79977cb0ec2f1f8aae31
Ковылин Алексей <alkovylin@ya.ru>
2022-05-24 17:08:20 +0300

    Добавьте файлы проекта.

---
+++ Configurations/Heating.cs
@@ -0,0 +1,13 @@
+﻿namespace HomeApi.Configurations
+{
+    /// <summary>
+    /// Тип отопления
+    /// </summary>
+    public enum Heating
+    {
+        None,
+        Oven,
+        Gas,
+        Electric,
+    }
+}
---
+++ Configurations/Material.cs
@@ -0,0 +1,12 @@
+﻿namespace HomeApi.Configurations
+{
+    /// <summary>
+    /// Материал изготовления
+    /// </summary>
+    public enum Material
+    {
+        Stone,
+        Wood,
+        Brick
+    }
+}
