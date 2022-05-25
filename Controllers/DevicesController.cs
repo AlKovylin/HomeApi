db230d896b2b166c6529071c425c0d27d8778f3e
Ковылин Алексей <alkovylin@ya.ru>
2022-05-25 16:59:57 +0300

    dff

---
+++ Controllers/DevicesController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HomeApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DevicesController : Controller
+    {
+        private readonly IHostEnvironment _env;
+
+        public DevicesController(IHostEnvironment env)
+        {
+            _env = env;
+        }
+
+
+        /// <summary>
+        /// Поиск и загрузка инструкции по использованию устройства
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [HttpHead]
+        [Route("{manufacturer}")]
+        public IActionResult GetManual([FromRoute] string manufacturer)
+        {
+            var staticPath = Path.Combine(_env.ContentRootPath, "Static");
+
+            var filePuth = Directory.GetFiles(staticPath)
+                .FirstOrDefault(f => f.Split("\\")
+                .Last()
+                .Split('.')[0] == manufacturer);
+
+            if (string.IsNullOrEmpty(filePuth))
+                return StatusCode(404, $"Инструкция '{manufacturer}' не найдена.");
+
+            string fileType = "application/pdf";
+            string fileName = $"{manufacturer}.pdf";
+
+            return PhysicalFile(filePuth, fileType, fileName);
+        }
+    }
+}
